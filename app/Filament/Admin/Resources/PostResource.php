b5e54a6ca80230e5b91e6b51fc3451d6b96b6d8a
anvarahmedov <144146721+anvarahmedov@users.noreply.github.com>
2025-04-12 21:37:35 +0000

    some commit

--- app/Filament/Admin/Resources/PostResource.php
+++ app/Filament/Admin/Resources/PostResource.php
@@ -56,7 +56,7 @@ class PostResource extends Resource
 TiptapEditor::make('body')
 ->required()
 ->directory('posts/media')
-->disk('s3'), // ✅ This works on TiptapEditor
+->disk('s3') // ✅ This works on TiptapEditor
 ->columnSpanFull(),
                     ]
                 )->columns(2),
